5718d51c7ecbf98b69c17e3bab3fa624e0078cbc
Roman Ataman <roman.ataman@adorsys.de>
2017-04-29 18:58:22 +0200

    Create empty Spring Boot project
    + Spring Data
    + Postgres

---
+++ src/main/java/de/gruene/HaustuerappServicesApplication.java
@@ -0,0 +1,13 @@
+package de.gruene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaustuerappServicesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HaustuerappServicesApplication.class, args);
+  }
+}
+
